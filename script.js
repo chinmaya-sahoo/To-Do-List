3cff7f9dda4ecaa2c8622832b0307eb2b86f7d05
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-08-17 23:10:10 +0530

    files added

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list_container');
+const button = document.querySelector('button');
+
+button.addEventListener('click',() => {
+    if (inputBox.value === ''){
+        alert('Please enter a valid task');
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+    inputBox.value = "";
+});
